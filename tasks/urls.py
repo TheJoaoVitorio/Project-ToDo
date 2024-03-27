97c3722f891a992b47da5aedfa9d96fcc3284d1f
TheJoaoVitorio <joaovtr999x@gmail.com>
2024-03-26 22:11:28 -0300

    criei a view para editar as task

--- tasks/urls.py
+++ tasks/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('',views.taskList,name='task-list'),
     path('task/<int:id>',views.taskView,name='task-view'),
     path('newtask/', views.newTask,name='new-task'),
+    path('edittask/<int:id>',views.editTask,name='edit-task'),
 ]
